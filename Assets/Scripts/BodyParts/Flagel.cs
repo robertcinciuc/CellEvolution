35297d364ebea0721143a0f3ea638cbaeea06577
Robert Cinciuc <robertcinciuc@gmail.com>
2022-04-03 14:29:50 +0200

    added flagel script to flagel prefab

---
+++ Assets/Scripts/BodyParts/Flagel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flagel : BodyPart
+{
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}

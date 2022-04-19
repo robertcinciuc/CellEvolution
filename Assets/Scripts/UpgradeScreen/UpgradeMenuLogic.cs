486051eda2a30fce08ec7269f2fa9776d5bfe136
Robert Cinciuc <robertcinciuc@gmail.com>
2022-04-19 19:10:38 +0200

    refactored menu logic instantiation (wokring state)

--- Assets/Scripts/UpgradeScreen/UpgradeMenuLogic.cs
+++ Assets/Scripts/UpgradeScreen/UpgradeMenuLogic.cs
@@ -9,6 +9,8 @@ public class UpgradeMenuLogic : MonoBehaviour
     private static Vector3 ogPlayerPos;
     private static Quaternion ogPlayerRot;
     private static GameObject playerMouth;
+    private static GameObject playerFlagel;
+    private static GameObject playerSpike;
 
     void Start(){
         player = GameObject.Find("Player");
@@ -27,8 +29,8 @@ public class UpgradeMenuLogic : MonoBehaviour
         ogPlayerRot = player.transform.rotation;
         player.transform.position = upgradeMenuPlane.transform.position + new Vector3(0, 1, 0);
         player.transform.rotation = new Quaternion(1, 0, 0, 0);
-        playerMouth = Instantiate((GameObject)Resources.Load("Prefabs/Mouth", typeof(GameObject)), player.transform.position + new Vector3(2, 0, 2), Quaternion.identity);
-        playerMouth = Instantiate((GameObject)Resources.Load("Prefabs/Flagel", typeof(GameObject)), player.transform.position + new Vector3(2, 0, 0), new Quaternion(0.71f, 0, 0, 0.71f));
+        
+        instMenuBodyParts();
     }
 
     public static void movePlayerToWorld() {
@@ -36,6 +38,19 @@ public class UpgradeMenuLogic : MonoBehaviour
         player.transform.rotation = ogPlayerRot;
         ogPlayerPos = Vector3.zero;
         ogPlayerRot = Quaternion.identity;
+
+        destroyMenuBodyParts();
+    }
+
+    private static void instMenuBodyParts() {
+        playerMouth = Instantiate((GameObject)Resources.Load("Prefabs/Mouth", typeof(GameObject)), player.transform.position + new Vector3(2, 0, 2), Quaternion.identity);
+        playerFlagel = Instantiate((GameObject)Resources.Load("Prefabs/Flagel", typeof(GameObject)), player.transform.position + new Vector3(2, 0, 0), new Quaternion(0.71f, 0, 0, 0.71f));
+        playerSpike = Instantiate((GameObject)Resources.Load("Prefabs/Spike", typeof(GameObject)), player.transform.position + new Vector3(2, 0, -2), new Quaternion(0.71f, 0, 0, 0.71f));
+    }
+
+    private static void destroyMenuBodyParts() {
         Destroy(playerMouth);
+        Destroy(playerFlagel);
+        Destroy(playerSpike);
     }
 }

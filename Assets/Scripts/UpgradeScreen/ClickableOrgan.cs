a9dd363ba65e712b5b0241d330b5cf7face4c635
Robert Cinciuc <robertcinciuc@gmail.com>
2022-05-08 16:14:02 +0200

    added logic for upgrade menu organ to follow mouse

--- Assets/Scripts/UpgradeScreen/ClickableOrgan.cs
+++ Assets/Scripts/UpgradeScreen/ClickableOrgan.cs
@@ -9,18 +9,28 @@ public class ClickableOrgan : MonoBehaviour
     public System.Type organType;
     public GameObject organ;
 
+
+    private Vector3 mousePosition;
+    private float moveSpeed = 0.1f;
+
     void Start(){
-        
+        mousePosition = new Vector3(0, 0, 0);        
     }
 
     void Update(){
-        
     }
 
     void OnMouseOver() {
         if (Input.GetMouseButtonDown(0)) {
             player.GetComponent<PlayerBodyStructure>().setOrganByType(organType, organ);
             playerCopy.GetComponent<PlayerBodyStructure>().setOrganByType(organType, organ);
+        } else {
+            if (Input.GetMouseButton(1)) {
+                Camera camera1 = GameObject.Find("UpgradeMenuCamera").GetComponent<Camera>();
+                mousePosition = camera1.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            }
         }
     }
+
 }

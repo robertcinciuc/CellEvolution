b46aebdf06b1a0c8e864f6ceb6cc75de88a209a2
Robert Cinciuc <robertcinciuc@gmail.com>
2022-06-23 11:15:18 +0200

    added joints (not working)

--- Assets/Scripts/Player/SegmentedBody.cs
+++ Assets/Scripts/Player/SegmentedBody.cs
@@ -12,14 +12,14 @@ public class SegmentedBody : MonoBehaviour{
 
         for (int i = 0; i < nbSegments; i++) {
             Vector3 segmentPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - i * 2); 
-            GameObject organ = Instantiate((GameObject)Resources.Load("Prefabs/Body", typeof(GameObject)), segmentPos, transform.rotation);
+            GameObject organ = Instantiate((GameObject)Resources.Load("Prefabs/PlayerBody", typeof(GameObject)), segmentPos, transform.rotation);
             organ.AddComponent<Rigidbody>();
             
             if(i == 0) {
                 organ.AddComponent<HeadMovement>();
             } else {
-                FollowerSegmentMovement followerSegmentMovement = organ.AddComponent<FollowerSegmentMovement>();
-                followerSegmentMovement.head = ((GameObject)segments[i-1]).transform;
+                CharacterJoint characterJoint = organ.AddComponent<CharacterJoint>();
+                characterJoint.connectedBody = ((GameObject)segments[i - 1]).GetComponent<Rigidbody>();
             }
             segments.Add(organ);
         }

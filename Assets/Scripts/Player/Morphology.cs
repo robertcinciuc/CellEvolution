567ab8fb276d3c884e4a5203383ba8643753f16e
Robert Cinciuc <robertcinciuc@gmail.com>
2022-07-12 21:43:41 +0200

    removed segmentedBody

--- Assets/Scripts/Player/Morphology.cs
+++ Assets/Scripts/Player/Morphology.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Morphology : MonoBehaviour
 {
-    public SegmentedBody segmentedBody;
     public GameObject playerHead;
 
     private Dictionary<System.Guid, GameObject> playerOrgans;
@@ -202,7 +201,7 @@ public class Morphology : MonoBehaviour
         playerSegments.Add(segmentComponent.segmentId, playerHead);
         segmentOrgans.Add(segmentComponent.segmentId, new Dictionary<System.Guid, GameObject>());
 
-        segmentedBody.initSegmentedBody(nbSegments);
+        initSegmentedBody();
 
         initPlayerOrgan(Mouths.Mouth.ToString(), "Prefabs/Mouth", new Vector3(0, 0, 0), Quaternion.identity, new Vector3(0, 0, 1), Quaternion.identity, typeof(Mouths), playerHead);
 
@@ -248,4 +247,43 @@ public class Morphology : MonoBehaviour
         return organ;
     }
 
+    private void initSegmentedBody() {
+        List<GameObject> segments = new List<GameObject>();
+
+        for (int i = 0; i < nbSegments; i++) {
+            Vector3 segmentPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - (i + 1) * 2);
+            GameObject segment = Instantiate((GameObject)Resources.Load("Prefabs/PlayerBody", typeof(GameObject)), segmentPos, transform.rotation);
+            segment.transform.parent = transform;
+            Rigidbody rigidbody = segment.AddComponent<Rigidbody>();
+
+            rigidbody.drag = 10;
+            rigidbody.mass = 0;
+
+            HingeJoint hingeJoint = segment.AddComponent<HingeJoint>();
+            if (i == 0) {
+                hingeJoint.connectedBody = playerHead.GetComponent<Rigidbody>();
+            } else {
+                hingeJoint.connectedBody = segments[i - 1].GetComponent<Rigidbody>();
+
+            }
+            hingeJoint.axis = Vector3.up;
+            hingeJoint.anchor = new Vector3(0, 0, 2);
+
+            hingeJoint.useLimits = true;
+            JointLimits limits = hingeJoint.limits;
+            limits.max = 20;
+            limits.min = -20;
+
+            hingeJoint.limits = limits;
+
+            //Add segment component
+            Segment segmentComponent = segment.AddComponent<Segment>();
+            segmentComponent.segmentId = System.Guid.NewGuid();
+            segmentComponent.segmentName = "PlayerBody";
+
+            addSegmentToList(segmentComponent.segmentId, segment);
+
+            segments.Add(segment);
+        }
+    }
 }

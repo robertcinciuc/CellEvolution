87db5583627871709174016a59a9a3ae86bffc47
Robert Cinciuc <robertcinciuc@gmail.com>
2022-10-12 21:48:42 +0200

    Added update to mesh + fixed indentation

--- Assets/Scripts/Player/MeshGenerator.cs
+++ Assets/Scripts/Player/MeshGenerator.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 
+
 public class MeshGenerator : MonoBehaviour {
   
-  void Start(){
-    gameObject.AddComponent<MeshFilter>();
-    gameObject.AddComponent<MeshRenderer>();
-    Mesh mesh = GetComponent<MeshFilter>().mesh;
+    private Mesh mesh;
+
+    void Start(){
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        mesh = GetComponent<MeshFilter>().mesh;
 	  
-	mesh.Clear();
+        mesh.Clear();
     
-    mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 0, 0)};
-    mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
-    mesh.triangles = new int[] {0, 1, 2};
-  }
+        mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 0, 0)};
+        mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        mesh.triangles = new int[] {0, 1, 2};
+    }
+
+    void Update() {
+        Vector3[] oldVertices = mesh.vertices;
+        Vector3[] newVertices = new Vector3[] { oldVertices[0] * 1.001f, oldVertices[1] * 1.001f, oldVertices[2] * 1.001f };
+        mesh.vertices = newVertices;
+    }
   
 }

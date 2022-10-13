ecd10f3c948417c3f582f699fb8b408d9afe37b9
Robert Cinciuc <robertcinciuc@gmail.com>
2022-10-13 18:04:45 +0200

    started implementation for drawing cylinder

--- Assets/Scripts/Player/MeshGenerator.cs
+++ Assets/Scripts/Player/MeshGenerator.cs
@@ -28,10 +28,24 @@ public class MeshGenerator : MonoBehaviour {
         mesh.Clear();
         
         // Add vertices 1 from segm1, then 1 from segm2
-        // mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 0, 0)};
+        if(vertices1.Length != vertices2.Length){
+            Debug.LogError("Slice vertices are of different sizes " + vertices1.Length + " " + vertices2.Length);    
+        }
+        List<int> concatVertices = new List<int>();
+        concatVertices.AddRange(vertices1);
+        concatVertices.AddRange(vertices2);
+        mesh.vertices = concatVertices.toArray();
+        
         // Calculate normals for UVs
         // mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        
         // Connect vertices into triangles
+        int[] triangleIndices = new int[(2 * vertices1.Length - 2) * 3];
+        for(int i = 0; i < vertices1.Length; i+=3){
+            triangleIndices[i] = concatVertices[i];
+            triangleIndices[i+1] = concatVertices[i + vertices1.Length - 1];
+            // triangleIndices[i+2]
+        }
         // mesh.triangles = new int[] {0, 1, 2};
     }
 }
